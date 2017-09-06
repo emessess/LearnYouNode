39ac4fb12e6aea2e87adb868e87ba00ed7333751
Matthew Smith <mattsheridansmith@gmail.com>
2017-09-06 17:18:30 -0400

    ex 1-5

---
+++ babysteps.js
@@ -0,0 +1,1 @@
+console.log(process.argv.slice(2).reduce((sum, arg) => sum + Number(arg), 0));
---
+++ filteredls.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+const dir = process.argv[2];
+fs.readdir(dir, (err, list) => {
+  if (err)
+    throw err;
+  const filteredls = list.filter(item => path.extname(item).slice(1) === process.argv[3]);
+  filteredls.forEach(item => console.log(item));
+  }
+);
